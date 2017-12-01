68406ff79357c3583f03ac4f678e6a9a86c8cfe2
Dario Lombardo <lomato@gmail.com>
2017-12-01 14:40:19 +0100

    add the actual migration to users.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171201113342) do
+ActiveRecord::Schema.define(version: 9) do
 
   create_table "logs", force: :cascade do |t|
     t.string "message", limit: 1000
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20171201113342) do
     t.string "extern_uid"
     t.integer "roles_mask"
     t.string "department"
+    t.boolean "active"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
