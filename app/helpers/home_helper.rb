9c03f5d2020c45e4d941590c78811ab0e2d17037
Dario Lombardo <lomato@gmail.com>
2017-12-03 23:57:22 +0100

    home: make proper rendering in home on mobile.

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,12 @@
 module HomeHelper
+	def home_buttons
+		link_to('Segna', presence_day_path, class: home_button_classes) +
+		link_to('Scegli giorno', presence_select_path, class: home_button_classes) +
+		link_to('Mostra', presence_show_path, class: home_button_classes) +
+		link_to('Statistiche', presence_stats_path, class: home_button_classes)
+	end
+
+	def home_button_classes
+		@classes ||= 'btn btn-lg btn-primary'
+	end
 end
