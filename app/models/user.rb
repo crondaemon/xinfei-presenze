0a8f6841be8356c638ad80ae1d917ffd971df9e0
Dario Lombardo <lomato@gmail.com>
2018-11-25 21:34:46 +0100

    user: don't force values, and allow setup of any field.
    Fix: #1.

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,21 +14,8 @@ class User < ActiveRecord::Base
 
   validates :username, presence: true, uniqueness: { scope: :provider, case_sensitive: false }
 
-  before_validation :fix_values
-
   def display_name
     fullname.presence || username
-  end
-
-  def fix_values
-    self.fullname = self.fullname.split.map(&:capitalize).join(' ')
-    
-    self.username = fullname.gsub(" ", '').downcase if !self.username || self.username.empty?
-    if !self.password
-      self.password = SecureRandom.hex(16)
-      self.password_confirmation = self.password
-    end
-    self.email = "#{self.username}@xinfei.org" if !self.email || self.email.empty?
   end
 
   # Devise method overridden to allow sign in with email or username
